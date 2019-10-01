45ef6084759a8944637948df432bf5fca051feef
johncolecarter <colecarter7777@gmail.com>
2019-09-30 23:51:25 -0500

    updated dash

--- myPortfolio/js/dashboard.js
+++ myPortfolio/js/dashboard.js
@@ -1,16 +1,52 @@
-$(function() {
-  let $list = $("ol");
-  let $newItemForm = $("#newItemForm");
+const addButton = document.querySelector(".addButton");
+var input = document.querySelector(".input");
+const container = document.querySelector(".container");
 
-  $newItemForm.on("submit", function(e) {
-    e.preventDefault();
-    let mytext = $('input[type="text"]').val();
-    $list.append(`<li>${mytext}</li>`);
-    $('input[type="text"]').val("");
-  });
+class item {
+  constructor(itemName) {
+    this.createDiv(itemName);
+  }
+  createDiv(itemName) {
+    let input = document.createElement("input");
+    input.value = itemName;
+    input.disabled = true;
+    input.classList.add("item_input");
+    input.type = "text";
 
-  $list.on("click", "li", function() {
-    let $this = $(this);
-    $this.remove();
-  });
-});
+    let itemBox = document.createElement("div");
+    itemBox.classList.add("item");
+
+    let editButton = document.createElement("button");
+    editButton.innerHTML = "EDIT";
+    editButton.classList.add("editButton");
+
+    let removeButton = document.createElement("button");
+    removeButton.innerHTML = "REMOVE";
+    removeButton.classList.add("removeButton");
+
+    container.appendChild(itemBox);
+
+    itemBox.appendChild(input);
+    itemBox.appendChild(editButton);
+    itemBox.appendChild(removeButton);
+
+    editButton.addEventListener("click", () => this.edit(input));
+
+    removeButton.addEventListener("click", () => this.remove(itemBox));
+  }
+
+  edit(input) {
+    input.disabled = !input.disabled;
+  }
+  remove(item) {
+    container.removeChild(item);
+  }
+}
+
+function check() {
+  if (input.value != "") {
+    new item(input.value);
+    input.value = "";
+  }
+}
+addButton.addEventListener("click", check);
